f92025f2ece23025f8e27a60060bffedab6de24b
LittleLW8847 <31029060+LittleLW8847@users.noreply.github.com>
2025-09-22 21:38:15 +0700

    step 4 complete
    clean up the code. making the "new vector3 (0,5,-7)" into a variable for ease of reading and not hard code it into the function

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
+    private Vector3 offset = new Vector3(0, 5, -6.74f);
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,6 +14,6 @@ public class FollowPlayer : MonoBehaviour
     void Update()
     {
         //offset camera to be behind the car
-        transform.position = player.transform.position + new Vector3(0,5,-7);
+        transform.position = player.transform.position + offset;
     }
 }
