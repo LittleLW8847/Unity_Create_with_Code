8771cf4b223188ea64b9563b9c32544ff6f2c9e9
LittleLW8847 <31029060+LittleLW8847@users.noreply.github.com>
2025-09-22 22:01:15 +0700

    step 3 complete
    learn about Input fuction/class(?) in Unity and a way Unity engine read input.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ public class PlayerController : MonoBehaviour
 
     public float speed = 20;
     public float turnSpeed;
+    public float horizontalInput;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,8 +15,10 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
         //move Vehicle forward
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
-        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed);
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+        
     }
 }
