5ed50d4b073bd45d8eb61dd190c73a5632da0cf0
LittleLW8847 <31029060+LittleLW8847@users.noreply.github.com>
2025-10-07 23:06:02 +0700

    TPS and FPS is ADDED!!!!!!
    I ACTUALLY DID IT. I FREAKING CLEAR BONUS FEATURE:HARD WHICH IS CAMERA SWITCHING.

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -1,19 +1,44 @@
+using Unity.Mathematics;
 using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
-    private Vector3 offset = new Vector3(0, 5, -6.74f);
+    private Vector3 offset1 = new Vector3(0, 5, -6.74f);
+    private Vector3 offset2 = new Vector3(0.25f, 1.98f, 0.61f);
+    private bool thirdPerson = true;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            thirdPerson = !thirdPerson;
+        }
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
         //offset camera to be behind the car
-        transform.position = player.transform.position + offset;
+        CameraSwitch();
+    }
+
+    void CameraSwitch()
+    {
+        if (thirdPerson)
+        {
+            transform.position = player.transform.position + offset1;
+            transform.rotation = Quaternion.AngleAxis(21.513f, Vector3.right);
+        }
+        else
+        {
+            transform.position = player.transform.position + offset2;
+            transform.rotation = Quaternion.AngleAxis(0, Vector3.right); 
+        }
     }
 }
